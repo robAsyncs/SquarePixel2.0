38e551a36a32d78fa61943d0b298d408ceba389f
Robel Tadele <robelabiti@gmail.com>
2025-02-11 18:59:56 -0500

    basic setup and added ImageCard and ImageGallery

---
+++ Views/ImageCard.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SquarePixel.Views;
+
+public partial class ImageCard : UserControl
+{
+    public ImageCard()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Views/ImageGallery.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SquarePixel.Views;
+
+public partial class ImageGallery : UserControl
+{
+    public ImageGallery()
+    {
+        InitializeComponent();
+    }
+}
